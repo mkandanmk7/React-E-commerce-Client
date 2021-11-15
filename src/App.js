0c7994f5c1c83606c8e5e3cdee347f3c6f254460
muthu <mkandanmk7@gmail.com>
2021-11-15 11:28:32 +0530

    Home routes done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
